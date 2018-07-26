fdcfd56d304bc685faa9d692893afcf61a500c82
rhw0009 <31529810+rhw0009@users.noreply.github.com>
2018-07-25 19:39:41 -0500

    Reset Test branch; starting from scratch

--- src/com/UDPClient.java
+++ src/com/UDPClient.java
@@ -6,129 +6,25 @@ import java.util.*;
 import java.util.stream.IntStream;
 import java.lang.*;
 
-
 public class UDPClient {
-    public static void main(String args[]) throws Exception {
-        //DatagramSocket clientSocket = new DatagramSocket(10051);
-        DatagramSocket clientSocket = new DatagramSocket(8081);
-        InetAddress IPAddress = InetAddress.getByName("localhost");
 
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
+    private static final int CLIENT_PORT = 81;
 
-        //boolean receiveComplete = false;
-        //Sends HTTP connection request to the server.
-        //URL url = new URL("HTTP","localhost", 8080, "ExampleWebPage.html");
-        URL url = new URL("localhost");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        //con.setRequestProperty("ExampleWebPage.html", "HTTP/1.0");
-        con.setRequestMethod("GET");
-        int testResponse = con.getResponseCode();
-        System.out.println(testResponse);
-
-        System.out.println("Sending request method to server...");
-        //DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 10048);
-        //clientSocket.send(sendPacket);
-
-        Scanner sc = new Scanner(System.in);
-        double chanceToCorrupt = 0;
-        boolean validInput = false;
-        System.out.println("Please enter the chance for a packet to be corrupted (between 0.0 and 1.0)");
-        while (!validInput) {
-            chanceToCorrupt = sc.nextDouble();
-            if (chanceToCorrupt >= 0.0 && chanceToCorrupt <= 1.0) {
-                validInput = true;
-            }
-            else {
-                System.out.println("Please enter a value between 0.0 and 1.0");
-            }
+    public static void main() {
+        //intitialize
+        try {
+            DatagramSocket socket = new DatagramSocket(CLIENT_PORT);
+        } catch(SocketException socketE) {
+            System.out.println("Could not establish socket on port " + CLIENT_PORT + ".");
         }
-        System.out.println("Applying corruption to packets...");
 
-        //System.out.println(sc.nextLine());
-
-
-        //Print send
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        boolean eof = false;
-        Random rng = new Random();
-        IntStream randStream;
-        int[] randArray = {0};
-        int sequenceNum = 0;
-        //Receive header
-        Vector<DatagramPacket> packetList = new Vector(0);
-        while (!eof) {  //Receive packets
-            clientSocket.receive(receivePacket);
-            if (receivePacket.getLength() == 1 && receivePacket.getData() == null) eof = true;
-            else {
-                randStream = rng.ints(2, 1, 101); //Generates 2 random numbers [1,100]; first is chance to corrupt, second is used to determine number of corrupt bits
-                randArray = randStream.toArray();
-                if (randArray[0] <= chanceToCorrupt * 100) {
-                    receivePacket = gremlin(receivePacket, randArray[1]);
-                }
-                errorDetected(receivePacket, sequenceNum);
-            }
-            packetList.add(receivePacket);
-            sequenceNum++;
-        }
-        String modifiedSentence = new String(receivePacket.getData());
-        System.out.println("FROM SERVER:" + modifiedSentence); //Print receive
-        clientSocket.close();
-    }
-
-    public static DatagramPacket gremlin(DatagramPacket packetIn, int distribution) {
-        DatagramPacket packet = packetIn;
-        int numToCorrupt;
-        if (distribution > 50) numToCorrupt = 1;
-        else if (distribution > 20) numToCorrupt = 2;
-        else numToCorrupt = 3;
-        Random rng = new Random();
-        boolean noDuplicates = true;
-        int[] randArray = {0};
-        IntStream randStream;
-        while (noDuplicates) {
-            randStream = rng.ints(numToCorrupt, 0, packet.getLength());
-            randArray = randStream.toArray();
-            for (int i = 0; i < numToCorrupt; i++) {
-                for (int j = i + 1; j < numToCorrupt; j++) {
-                    if (randArray[i] == randArray[j]) {
-                        noDuplicates = false;
-                    }
-                }
-            }
-        }
-        byte[] packetData = packet.getData();
-        byte dataByte;
-        int dataInt;
-        byte ones = 0b1111111;
-        byte zeroes = 0b00000000;
-        for (int i = 0; i < randArray.length; i++) {
-            int byteIndex = randArray[i];
-            dataByte = packetData[byteIndex];
-            dataInt = (int) dataByte;
-            dataInt = dataInt ^ ones;
-            dataInt = dataInt ^ zeroes;
-            packetData[i] = (byte) dataInt;
-        }
-        packet.setData(packetData);
-        return packet;
-    }
-
-    public static boolean errorDetected(DatagramPacket packet, int sequenceNum) {
-        int checksum = 0;
-        int cSumStartIndex = (packet.getData()).toString().indexOf("Checksum: ");
-        cSumStartIndex += 10;
-        int cSumEndIndex = (packet.getData()).toString().indexOf(' ', cSumStartIndex);
-        String cSumString = (packet.getData()).toString().substring(cSumStartIndex, cSumEndIndex);
-        int correctSum = Integer.parseInt(cSumString);
-        for (int i = 0; i < packet.getLength(); i++) {
-            checksum += packet.getData()[i];
-        }
-        if (checksum == correctSum) {
-            return false;
-        } else {
-            System.out.println("Error detected in packet " + sequenceNum);
-            return true;
-        }
+        //send request
+        //receive packets
+        //gremlin
+        //error detection
+        //send ACK/NAKs
+        //receive missing packets
+        //assemble packets
+        //print output
     }
 }
--- src/com/UDPServer.java
+++ src/com/UDPServer.java
@@ -5,92 +5,13 @@ import java.net.*;
 import java.util.Vector;
 
 class UDPServer {
-    public static void main(String args[]) throws Exception
-    {
-        //DatagramSocket serverSocket = new DatagramSocket(10048);
-        DatagramSocket serverSocket = new DatagramSocket(8080);
-        byte[] receiveData = new byte[1024];
-        byte[] sendData  = new byte[1024];
-        byte[] endData = new byte[1];
-        int checksum = 1024;
-        endData = null;
-
-        //Should receive the first packet that the client sends
-        System.out.println("Receiving Data from client... ");
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        serverSocket.receive(receivePacket);
-
-        String sentence = new String(receivePacket.getData());
-        InetAddress IPAddress = receivePacket.getAddress();
-
-        // Should print out whatever data the client first sends.
-        System.out.println(receiveData.toString());
-
-        System.out.println("getting the clients port number in order to return the message.");
-        int port = receivePacket.getPort();
-
-
-        // This is the packet header. This should turn the String into bytes ready to send via datagram sendPacket
-        String packetHeader = ("http/1.0 200 document follows\r\n " +
-                    "content-type: text/plain \r\n " +
-                    "Content Length: 1024 bytes \r\n\r\n" +
-                    "Checksum: " + checksum);
-        sendData = packetHeader.getBytes();
-
-        System.out.println("responding to the clients request...");
-
-
-        // Loop that is supposed to split the response packet into 4 and sends back to the client
-        int i = 0;
-         while(i < 4)
-         {
-             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length / 4, IPAddress, port);
-             serverSocket.send(sendPacket);
-             getChecksum(sendPacket);
-             i++;
-         }
-
-
-            // This is suppose to send At the end of the file,
-            // it transmits 1 byte(NULL character) that indicates the end of the file.
-            // It will then close the file.
-            System.out.println("Sending the 1 byte packet to identify the end of the message...");
-            DatagramPacket sendEndPacket = new DatagramPacket(endData, endData.length, IPAddress, port);
-            serverSocket.send(sendEndPacket);
-            serverSocket.close();
-
-    }
-
-    public static int getChecksum(DatagramPacket packet) {
-        int checksum = 0;
-        for (int i = 0; i < packet.getLength(); i++) {
-            checksum += packet.getData()[i];
-        }
-        return checksum;
-    }
-
-    public static Vector<String> generatePackets(String filename) {
-        Vector<String> output = new Vector<String>(0);
-        try {
-            FileInputStream file;
-            try {
-                file = new FileInputStream(filename);
-            } catch (FileNotFoundException fileNotFound) {
-                System.out.println("Error: file not found.");
-                return null;
-            }
-            boolean eof = false;
-            int numRead = 0;
-            byte[] currentPacket = {};
-            while (!eof) {
-                numRead = file.read(currentPacket, 0, 256);
-                if (numRead < 256) eof = true;
-                output.add(currentPacket.toString());
-            }
-            output.add("\0"); //add eof null packet
-        } catch(IOException ioExcept) {
-            return null;
-        }
-        return output;
+    public static void main() {
+        //initialize
+        //receive request
+        //find file
+        //generate packets
+        //send packets
+        //receive ACK/NAKs
+        //resend missing packets
     }
 }
